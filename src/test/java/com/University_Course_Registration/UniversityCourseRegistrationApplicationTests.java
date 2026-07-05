6ce79d557c14c6484937c8ffaab43d87a85d2db2
Caisha Mohamud <caisha.maxamud09@gmail.com>
2026-07-05 22:25:59 +0300

    init commit

---
+++ src/test/java/com/University_Course_Registration/UniversityCourseRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.University_Course_Registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCourseRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
